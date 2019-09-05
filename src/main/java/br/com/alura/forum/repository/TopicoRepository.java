4545f8fb97a4e134d3a375bb56a519c555b871e9
Paulo Henrique Correa dos Santos <phcorreacontato@gmail.com>
2019-09-04 23:26:51 -0300

    Spring Boot 1 - criação da api com os principais métodos e validações iniciais

---
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,20 @@
+package br.com.alura.forum.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.alura.forum.model.Topico;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	//Seguindo a convenção do Spring
+	List<Topico> findByCursoNome(String nomeCurso);
+	
+	//Montando a Query na mão com JPQL
+	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+	List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String cursoNome);
+
+}
